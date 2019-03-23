8bce9b199c9ff566fb7a51badf2813cd32313c34
Luis Zabala <luis.zabala076@gmail.com>
2019-03-23 21:24:31 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,7 +1,14 @@
 class CouponsController < ApplicationController
   
   def index
+    @coupon = Coupons.all
+  end
+  
+  def new
+  end
+  
+  def create
+    @coupon = Coupon.create(:coupon_code params[:coupon_code],)
+  
 end
-
-
 end
