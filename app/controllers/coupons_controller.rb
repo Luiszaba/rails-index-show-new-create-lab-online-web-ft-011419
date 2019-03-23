0d6981cd61be4da52d3b0f67068c180fb6856dc1
Luis Zabala <luis.zabala076@gmail.com>
2019-03-23 21:26:21 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,6 +12,7 @@ class CouponsController < ApplicationController
       coupon_code: params[:coupon][:coupon_code], 
       store: params[:coupon][:store])
     @coupon.save
+    redirect_to coupon_path(coupon)
   end
 end
   
